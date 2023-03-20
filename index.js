571ea71a792f5007681c31c8fa67ebe404b13bb8
Gulica Silvian <gulica.sv@gmail.com>
2023-03-21 01:09:04 +0200

    Added html files

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const http = require("node:http")
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end("Hello World!");
-})
+const http = require('http');
+const fs = require('fs');
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000!");
-})
+const server = http.createServer(function(req, res) {
+  if (req.method === 'GET' && req.url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('index.html', function(err, data) {
+      if (err) {
+        res.statusCode = 500;
+        res.end('Error reading index.html');
+      } else {
+        res.statusCode = 200;
+        res.end(data);
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
+  }
+});
+
+// Listen on port 3000
+server.listen(3000);
