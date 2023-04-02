c400f33cbc7703bdb4f3912f3854f99a9a845ea5
Claudiu Popoiu <claupop2002@gmail.com>
2023-04-02 06:12:06 +0300

    STYLE: Update login page

--- server/login.js
+++ server/login.js
@@ -1,5 +1,15 @@
-const signUpAnchor = document.querySelector(".signup__link");
-
-signUpAnchor.addEventListener("click", () => {
-  window.location.href = "http://localhost:3000/signupssss";
+const loginPanelFooterAnchor = document.querySelector(
+  ".login-panel__footer-anchor"
+);
+const signupPanelFooterAnchor = document.querySelector(
+  ".sign-up-panel__footer-anchor"
+);
+const panel = document.querySelector(".panel");
+const logInPanel = document.querySelector(".login-panel");
+loginPanelFooterAnchor.addEventListener("click", () => {
+  panel.classList.toggle("panel--flip");
 });
+signupPanelFooterAnchor.addEventListener("click", () => {
+  panel.classList.toggle("panel--flip");
+});
+//work in progress
