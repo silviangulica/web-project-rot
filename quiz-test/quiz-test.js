f58437c2d40527a8a5856c588db97d5dfb9b767f
Claudiu Popoiu <54360338+Claudiu2222@users.noreply.github.com>
2023-06-20 20:27:58 +0300

    FEAT: Timer demo

--- quiz-test/quiz-test.js
+++ quiz-test/quiz-test.js
@@ -1,3 +1,22 @@
+let countDownDate = new Date().getTime() + 1000 * 60 * 30;
+
+(async () => {
+  await checkIfUserAuthDidNotExpire();
+  // if (JSON.parse(localStorage.getItem("currentQuiz")) == null) {
+  //   window.location.href = "../dashboard/dashboard.html";
+  // }
+  setInterval(() => {
+    const timer = document.querySelector(".quiz__start-timer");
+    let now = new Date().getTime();
+    let time = countDownDate - now;
+    let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((time % (1000 * 60)) / 1000);
+    document.querySelector("#remaining-time").textContent = `${minutes}:${
+      seconds > 9 ? seconds : "0" + seconds
+    }`;
+  }, 1000);
+})();
+
 function changeNavBarColor() {
   const navbar = document.querySelector(".nav");
 
