893ed447fb544bac7aaad0e00477832be6a458b3
Gulica Silvian <gulica.sv@gmail.com>
2023-06-21 20:45:11 +0300

    FIX: Lesson link

--- lesson/lesson.js
+++ lesson/lesson.js
@@ -1,3 +1,32 @@
+const fancyAlert = (msg) => {
+  // Creeam un div nou
+  let alert = document.createElement("div");
+  alert.classList.add("alert");
+  alert.innerHTML = msg;
+  // Adaugam div-ul la body
+  document.body.appendChild(alert);
+
+  // Facem un style frumos la div
+  alert.style.position = "fixed";
+  alert.style.top = "10px";
+  alert.style.left = "50%";
+  alert.style.transform = "translateX(-50%)";
+  alert.style.padding = "10px 20px";
+  alert.style.borderRadius = "5px";
+  alert.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+  alert.style.color = "white";
+  alert.style.fontFamily = "sans-serif";
+  alert.style.fontSize = "20px";
+  alert.style.fontWeight = "bold";
+  alert.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)";
+  alert.style.zIndex = "1000";
+
+  // Stergem div-ul dupa 3 secunde
+  setTimeout(() => {
+    document.body.removeChild(alert);
+  }, 3000);
+};
+
 function changeNavBarColor() {
   const navbar = document.querySelector(".nav");
 
@@ -11,56 +40,142 @@ function changeNavBarColor() {
 window.addEventListener("scroll", changeNavBarColor);
 // -- End Pretty navbar
 
-// // -- Start dom manipulation
-// const lesson_title = document.querySelector(".lesson__title")
-// const lesson_content = document.querySelector(".lesson__content-text")
-// const lesson_image = document.querySelector(".lesson__image--visible")
+// -- Start dom manipulation
+const lesson_panel = document.querySelector(".lesson");
+lesson_panel.style.display = "none";
 
-// let lesson = {
-//   title: "",
-//   description: "",
-//   image_url: ""
-// }
+const cards = document.querySelectorAll(".card");
+let lessons = [];
 
-// var lesson_id = 1
-// let url = "http://127.0.0.1:8081"
+cards.forEach((card) => {
+  lessons = [];
+  let categorie = card.dataset.categorie;
+  card.addEventListener("click", () => {
+    fetch(`http://localhost:8081/lessons/type/?type=${categorie}`)
+      .then((response) => response.json())
+      .then((data) => {
+        lessons = data;
+        if (lessons.length > 0) {
+          handelLessons(lessons);
+        }
+      });
+  });
+});
 
-// function getLesson(lesson_id) {
-//   console.log(lesson_id)
-//   fetch(url + `/lessons/${lesson_id}`)
-//     .then(response => response.json())
-//     .then(data => {
-//       lesson = {
-//         title: data.title,
-//         description: data.description,
-//         image_url: data.image_url
-//       }
+let currentLesson = null;
 
-//       lesson_title.innerHTML = lesson.title
-//       lesson_content.innerHTML = lesson.description
-//       lesson_image.src = lesson.image_url
+function revertToCategories() {
+  // toata clasa lessons__categories trebuie sa reapara, display block
+  let lessons_categories = document.querySelectorAll(".card");
+  lessons_categories.forEach((categorie) => {
+    categorie.style.display = "block";
+  });
 
-//       console.log(lesson)
-//     }
-//   )
-// }
+  lesson_panel.style.display = "none";
 
-// getLesson(lesson_id)
+  let lessons_back = document.querySelector(".lessons__back");
+  lessons_back.style.display = "none";
 
-// const next_button = document.querySelector(".lesson__panel-next-lesson")
+  let card_lessons = document.querySelectorAll(".card-lesson");
+  card_lessons.forEach((card_lesson) => {
+    card_lesson.remove();
+  });
+}
 
-// next_button.addEventListener("click", () => {
-//   lesson_id += 1
-//   getLesson(lesson_id)
-// })
+function handelLessons(lessons) {
+  // toata clasa lessons__categories trebuie sa dispara, display none
+  let lessons_categories = document.querySelectorAll(".card");
+  lessons_categories.forEach((categorie) => {
+    categorie.style.display = "none";
+  });
 
-// const previous_button = document.querySelector(".lesson__panel-prev-lesson")
+  let lessons_back = document.querySelector(".lessons__back");
+  lessons_back.style.display = "block";
 
-// previous_button.addEventListener("click", () => {
-//   lesson_id -= 1
-//   if (lesson_id < 1) {
-//     lesson_id = 1
-//   }
-//   getLesson(lesson_id)
-// })
-// -- End dom manipulation
+  // lessons_back este buton, ii adaugam functie
+  lessons_back.addEventListener("click", revertToCategories);
+
+  lessons.forEach((lesson) => {
+    // Create lesson card si il adaugam la clasa lessons
+    let lesson_card = document.createElement("div");
+    lesson_card.classList.add("card-lesson");
+
+    // Create lesson image
+    let lesson_image = document.createElement("img");
+    lesson_image.classList.add("card-lesson__img");
+    lesson_image.src = lesson.image_url;
+
+    // Create lesson title
+    let lesson_title = document.createElement("div");
+    lesson_title.classList.add("card-lesson__title");
+    lesson_title.innerHTML = lesson.title;
+
+    // Adaugam img si title la card
+    lesson_card.appendChild(lesson_image);
+    lesson_card.appendChild(lesson_title);
+
+    // Adaugam card la lessons
+    let lessons = document.querySelector(".lessons");
+    lessons.appendChild(lesson_card);
+
+    // Funciton to handle the click of the lesson card
+    lesson_card.addEventListener("click", () => {
+      createLessonFrom(lesson);
+      currentLesson = lesson;
+      // Hide all the cards
+      let card_lessons = document.querySelectorAll(".card-lesson");
+      card_lessons.forEach((card_lesson) => {
+        card_lesson.style.display = "none";
+      });
+
+      // Show the lesson
+      let realLesson = document.querySelector(".lesson");
+      realLesson.style.display = "flex";
+    });
+  });
+}
+
+let prev_button = document.querySelector(".lesson__panel-prev-lesson");
+let next_button = document.querySelector(".lesson__panel-next-lesson");
+
+prev_button.addEventListener("click", () => {
+  let lesson_index = lessons.findIndex(
+    (lesson) => lesson._id === currentLesson._id
+  );
+  if (lesson_index > 0) {
+    let prev_lesson = lessons[lesson_index - 1];
+    createLessonFrom(prev_lesson);
+    currentLesson = prev_lesson;
+  } else {
+    revertToCategories();
+    fancyAlert("Ai terminat acest capitol!");
+  }
+});
+
+next_button.addEventListener("click", () => {
+  let lesson_index = lessons.findIndex(
+    (lesson) => lesson._id === currentLesson._id
+  );
+  if (lesson_index < lessons.length - 1) {
+    let next_lesson = lessons[lesson_index + 1];
+    createLessonFrom(next_lesson);
+    currentLesson = next_lesson;
+  } else {
+    revertToCategories();
+    fancyAlert("Ai terminat acest capitol!");
+  }
+});
+
+const createLessonFrom = (lesson) => {
+  // Get the title of the lesson
+  let lesson_title = document.querySelector(".lesson__title");
+  lesson_title.innerHTML = lesson.title;
+
+  // Get the description of the lesson
+  let lesson_description = document.querySelector(".lesson__content-text");
+  lesson_description.innerHTML = lesson.description;
+
+  // Get the image of the lesson
+  let lesson_image = document.querySelector(".lesson__image--visible");
+  lesson_image.src = lesson.image_url;
+};
